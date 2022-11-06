e3b6fe443ed21056056a9c97ae627cda7d2ca982
AkaAugustoNeves <augusto.nevesti@gmail.com>
2022-11-06 15:53:54 -0300

    first commit

---
+++ src/main/java/br/com/pilares/msteste/MstesteApplication.java
@@ -0,0 +1,13 @@
+package br.com.pilares.msteste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MstesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MstesteApplication.class, args);
+	}
+
+}
